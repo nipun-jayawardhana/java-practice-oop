b0824b09a1b196b913bf9bffc44d7f2e9b404214
nipun-jayawardhana <nipun.madhushanka1999@gmail.com>
2025-04-13 16:00:02 +0530

    adding packags and how to working between them

---
+++ src/main/java/Pack1/Kamal.java
@@ -0,0 +1,11 @@
+package Pack1;
+import Pack2.Saaman;
+
+public class Kamal {
+
+
+    public static void main(String[] args) {
+        Nimal obj1=new Nimal();
+        Saaman obj2=new Saaman();
+    }
+}
